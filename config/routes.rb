aba4c38dc8b49ceadc221ec58f0269b37f8c1936
Schoorsteenveger <janoutreffers@gmail.com>
2017-04-05 12:45:27 +0200

    3.8 add description of change

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'welcome#index'
-  root 'static_pages#index'
+
 end
