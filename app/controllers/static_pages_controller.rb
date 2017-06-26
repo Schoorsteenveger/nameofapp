b885acfbdbd7baaa1d09154f66926d2168b3dc69
Schoorsteenveger <janoutreffers@gmail.com>
2017-06-26 16:41:46 +0200

    5.1 add reroute and redirect

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,7 @@ class StaticPagesController < ApplicationController
   # GET /landing_page.json
   def landing_page
     @products = Product.limit(6)
+    redirect_to ('/static_pages/contact')
   end
 end
 
