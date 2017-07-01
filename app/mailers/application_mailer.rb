ff8c89891a67f925fb494ad355604a54d7703779
Schoorsteenveger <janoutreffers@gmail.com>
2017-07-01 15:17:00 +0200

    5.3 Add email template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,16 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'janoutreffers@gmail.com'
   layout 'mailer'
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+
+    ActionMailer::Base.mail(from: @email,
+        to: 'your-email@example.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 end
