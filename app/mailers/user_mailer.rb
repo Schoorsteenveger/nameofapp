3acc1d5454bdfc0c48bab237a3c9e0abc24c5f71
Schoorsteenveger <janoutreffers@gmail.com>
2017-07-03 14:13:55 +0200

    5.3 add emailtemplate

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "janoutreffers@gmail.com"
 
-  def contact_form(email, name, message)
-  @message = message
-    mail(from: email,
-         to: 'janoutreffers@gmail.com',
-         subject: "A new contact form message from #{name}")
-  end
-  
+    def contact_form(email, name, message)
+      @message = message
+
+      mail(from: email,
+           to: 'your-email@example.com',
+           subject: "A new contact form message from #{name}")
+    end
 end
