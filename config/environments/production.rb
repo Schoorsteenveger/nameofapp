8c2e7ba492b29ca31826063880f823d0853634ec
Schoorsteenveger <janoutreffers@gmail.com>
2017-07-08 16:02:52 +0200

    5.4 fix registration and styling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'happybikeshop-1.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
@@ -93,8 +96,6 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
-
-  config.action_mailer.default_url_options = { :host => 'https://bikeberlin77.herokuapp.com'}
 end
 
 
