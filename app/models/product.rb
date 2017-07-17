4d80bef939daf22d807535645fe35d70792410bd
Schoorsteenveger <janoutreffers@gmail.com>
2017-07-17 17:33:59 +0200

    5.7 add comments and styling showpage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,16 @@
 class Product < ApplicationRecord
   has_many :orders
-
+  has_many :comments
   def self.search(search_term)
     Rails.env.production? ? @products = Product.where("name ilike ?", "%#{search_term}%") : @products = Product.where("name LIKE ?", "%#{search_term}%")
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowerest_rating_comment
+    comments.rating_asc.first
   end
 end
 
