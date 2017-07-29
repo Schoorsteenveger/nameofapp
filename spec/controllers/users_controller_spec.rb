34fd5cccd4dab821b645c591860a6eb1a83d73c4
Schoorsteenveger <janoutreffers@gmail.com>
2017-07-29 14:29:19 +0200

    6.3 correct users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller  do
-  @user = User.create!(first_name: 'theo', last_name: 'treffers', email:'theotreffers@gmail.com', password: '1234567')
-  @user1 = User.create!(first_name: 'ollie', last_name: 'janssen', email: 'ollie@gmail.com', password: '5678900')
-end
+
+  before do
+  @user = User.create!(first_name: 'harry', last_name: 'treffers', email:'harrytreffers@gmail.com', password: '1234567')
+  @user1 = User.create!(first_name: 'han', last_name: 'janssen', email: 'han@gmail.com', password: '5678900')
+  end
 
   describe 'GET #show' do
     context 'when a user is logged in' do
       before do
-        sign_in @user
+        it "loads correct user details" do
+          expect(assigns(:user)).to eq @user
+          expect(response.status).to eq(200)
+        end
       end
-
-      it "loads correct user details" do
-        get :show, params: {id: @user.id}
-        expect(assigns(:user)).to eq @user
-        expect(response.status).to eq(200)
-      end
+    end      
 
     context 'when a user is not logged in' do
       it 'redirects to login' do
         get :show, params: {id: @user.id}
-        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to redirect_to(root_path)
       end
+    end
 
-    context "User1 cannot access User#Show" do
-      before do 
+    context "cannot access second user show page" do
+      it "redirects to root" do
         sign_in @user1
       end
 
       it "redirects to root_path" do
-        get :show, params: {id: @user1.id}
+        get :show, params: {id: @user1.id}        
+        expect(response).to have_http_status(302)
         expect(response).to redirect_to(root_path)
       end
-    end
     end
   end
 
