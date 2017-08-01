5e33953dd60bbe5577a9b7e5898da6af6fd43534
Schoorsteenveger <janoutreffers@gmail.com>
2017-08-01 14:50:18 +0200

    6.6 correct syntaxs tripemail

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,7 +12,7 @@ class PaymentsController < ApplicationController
         amount: (@product.price * 100).to_i, 
         currency: "eur",
         source: token,
-        description: params[:stripeEmail]
+        description: params[:stripeEmail],
         receipt_email: params[:stripeEmail]
       )
 
