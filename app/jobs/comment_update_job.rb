7df54fcd757f370ccb70a3d9a53f7f97dd117bd0
Schoorsteenveger <janoutreffers@gmail.com>
2017-08-18 13:54:49 +0200

    add Active Job

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -3,7 +3,8 @@ class CommentUpdateJob < ApplicationJob
 
   def perform(comment, current_user)
     # Do something later
-    ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating 
+    ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), 
+    average_rating: comment.product.average_rating)
   end
 
   private
