419dcc573df4af604f2c45ed106cb90fdc8c21fa
Schoorsteenveger <janoutreffers@gmail.com>
2017-09-23 16:40:56 +0200

    6.15 try fix redis and categoryview

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,11 +25,12 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    @product = Product.new
+    @product = Product.new @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   # GET /products/1/edit
   def edit
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
   # POST /products
   # POST /products.json
